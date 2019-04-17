2e3aeaa075e1e9701f39af69752da146d5ed7e42
bosakalaka <936285702@qq.com>
2019-04-17 16:23:48 +0800

    第一次提交-删除暂时用不到的配置

---
+++ src/ToduList.js
@@ -0,0 +1,24 @@
+// import React from "react";
+
+// class ToduList extends React.Component {
+//     render() {
+//         return (
+//             <h1>Hello World</h1>
+//         )
+//     }
+// }
+
+// export default ToduList;
+
+import React,{Component} from 'react';
+
+class TudoList extends Component {
+    render () {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default TudoList;
+
